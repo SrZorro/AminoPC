71c3b4c0240f1cc1512262e7ddd8e5cedd07b996
Fox <fox@foxdev.io>
2018-06-15 13:25:44 +0200

    Added some padding betwen textbox and last message

--- app/src/components/ThreadChat.tsx
+++ app/src/components/ThreadChat.tsx
@@ -196,7 +196,7 @@ export default class ThreadChat extends Component<any, any> {
                 </div>
                 <div class={classBody}>
                     {chatBubbles}
-                    <div ref={(el) => { this.dummyBottomDiv = el; }}></div>
+                    <div style={{ height: 10 }} ref={(el) => { this.dummyBottomDiv = el; }}></div>
                 </div>
                 {<div style={this.props.showScrollBtn ? { opacity: 1 } : { opacity: 0 }} class={classScrollBtn} onclick={() => this.scrollToBottom(false)}><svg class={classScrollBtnIcon} viewBox="0 0 512 512"><path style={{ transform: "translate(90px, 125px)" }} fill="currentColor" d="M314.5,90.5c0,6-2,13-7,18l-133,133c-5,5-10,7-17,7s-12-2-17-7l-133-133c-10-10-10-25,0-35 s24-10,34,0l116,116l116-116c10-10,24-10,34,0C312.5,78.5,314.5,84.5,314.5,90.5z"></path></svg></div>}
                 <div class={classFooter}>
