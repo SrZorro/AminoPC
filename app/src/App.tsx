91a5b4e25884c9f40e1a09e10bc7a1c313dd69b7
Fox <fox@foxdev.io>
2018-06-16 19:22:47 +0200

    Prevent droped files to open file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,4 +3,7 @@ import Amino from "./components/Amino";
 
 const container = document.getElementById("app");
 
+document.addEventListener("dragover", event => event.preventDefault())
+document.addEventListener("drop", event => event.preventDefault())
+
 render(<Amino />, container);
